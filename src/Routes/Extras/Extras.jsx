03d17b34b28dcaf01e862f15e1d506e2c925da4e
TheArthurVincent <arthurcardosocorp@gmail.com>
2023-10-11 16:21:25 -0300

    *

--- src/Routes/Extras/Extras.jsx
+++ src/Routes/Extras/Extras.jsx
@@ -3,7 +3,6 @@ import {
   RouteDiv,
   HOne,
   RouteSizeControlBox,
-  HTwo,
 } from "../../Resources/Components/RouteBox";
 import { useUserContext } from "../../Application/SelectLanguage/SelectLanguage";
 import {
@@ -11,9 +10,9 @@ import {
   IFrameVideo,
   logout24h,
 } from "../../Resources/UniversalComponents";
-import generalClasses from "../../assets/mockdata/universalcontent.json";
 import { transparentWhite } from "../../Styles/Styles";
 import TopBar from "../../Application/TopBar/TopBar";
+import { HThree } from "../MyClasses/MyClasses.Styled";
 
 export function Extras() {
   useEffect(() => {
@@ -22,15 +21,41 @@ export function Extras() {
 
   const { UniversalTexts } = useUserContext();
 
+  const contentExtras = [
+    {
+      instruction: "Como estudar",
+      url: "https://player.vimeo.com/video/852643150",
+      explanation:
+        "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ",
+    },
+    {
+      instruction: "Como usar o Anki",
+      url: "https://player.vimeo.com/video/852643150",
+      explanation:
+        "Lo2rem2 Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem ",
+    },
+    {
+      instruction: "Termos e condições",
+      url: "https://player.vimeo.com/video/852643150",
+      explanation:
+        "Lo2rem2 Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem ",
+    },
+    {
+      instruction: "Aulas ao vivo",
+      url: "https://player.vimeo.com/video/852643150",
+      explanation:
+        "Lo2rem2 Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem Lo2rem ",
+    },
+  ];
   return (
     <>
       <TopBar />
       <RouteSizeControlBox>
         <RouteDiv>
           <HOne>{UniversalTexts.extras}</HOne> <BackToHomePage />
-          {generalClasses.contentExtras.map((item, index) => (
+          {contentExtras.map((item, index) => (
             <div key={index}>
-              <HTwo>{item.instruction}</HTwo>
+              <HThree>{item.instruction}</HThree>
               <div style={{ textAlign: "center" }}>
                 <IFrameVideo src={item.url} frameBorder="0" />
               </div>
