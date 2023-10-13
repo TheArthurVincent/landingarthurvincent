b425f22178d788353e295c6d76ea0f52a61e8960
TheArthurVincent <arthurcardosocorp@gmail.com>
2023-10-13 13:38:43 -0300

    *

--- src/Routes/MyCourses/MyCourses.jsx
+++ src/Routes/MyCourses/MyCourses.jsx
@@ -66,6 +66,7 @@ export default function MyCourses() {
               })}
             </div>
           </div>
+          <BackToHomePage />
         </RouteDiv>
       </RouteSizeControlBox>
     </>
