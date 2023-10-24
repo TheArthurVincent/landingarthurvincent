51cff8ea0255ea944f4ca434d9407422e092cc01
TheArthurVincent <arthurcardosocorp@gmail.com>
2023-10-23 22:37:37 -0300

    *

--- src/Routes/ClassesToTeach/BasicTexts/BasicTextsPresentTense.jsx
+++ src/Routes/ClassesToTeach/BasicTexts/BasicTextsPresentTense.jsx
@@ -28,31 +28,31 @@ export function BasicTextsPresentTense() {
       title: "Text 1: Maria from Brazil",
       text: "Hello, everyone! My name is Maria. I'm from Brazil. I'm 25 years old. I work as a teacher. I'm single. I love spending time on the beach and dancing samba with my friends. Brazil is known for its beautiful beaches, and I'm lucky to live near one. Teaching is my passion, and I enjoy helping students learn and grow. In my free time, I also enjoy reading novels and trying out new recipes. Nice to meet you all!",
       srcAudio:
-        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639909467&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639909467&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false",
     },
     {
       title: "Text 2: Ahmed from Egypt",
       text: "Hi, there! I'm Ahmed, and I come from Egypt. I'm 30 years old. I work as an engineer. I'm married and have two lovely kids. I enjoy exploring historical sites and learning about ancient Egyptian culture. As an engineer, I find joy in designing and creating things that make life easier. In my free time, I like playing soccer with my friends and watching documentaries. I'm excited to be a part of this group!",
       srcAudio:
-        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639910796&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639910796&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false",
     },
     {
       title: "Text 3: Sophie from France",
       text: "Hello! I'm Sophie, and I'm French. I'm 28 years old. I work as a graphic designer. I'm single and enjoying every moment of my life. France is famous for its art, so I often visit museums. As a graphic designer, I love using colors and shapes to create beautiful designs. I also enjoy outdoor activities like hiking in the countryside.",
       srcAudio:
-        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639912275&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639912275&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false",
     },
     {
       title: "Text 4: Hiroshi from the US",
       text: "Hi! I'm Hiroshi from the United States. I'm 19 years old. I'm a student, studying computer science. I'm single and focused on my studies at the moment. My family is from Japan, and in Japan, I love practicing traditional tea ceremonies and learning about samurai history. Being a computer science student, I spend a lot of time coding and exploring new technologies. When I have free time, I like playing video games and going for walks in nature. Let's have a great time together!",
       srcAudio:
-        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639915080&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639915080&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false",
     },
     {
       title: "Text 5: Isabella from Mexico",
       text: "Hi, amigos! I'm Isabella, and I come from Mexico. I'm 35 years old. I work as a nurse. I'm happily married and have a son. In Mexico, I enjoy celebrating cultural festivals and cooking traditional dishes for my family. As a nurse, I find fulfillment in taking care of others and making a positive impact on their lives. During my free time, I like gardening and dancing to Mexican music. Excited to connect with you all!",
       srcAudio:
-        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639918404&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+        "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639918404&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false",
     },
   ];
   const professions = [
@@ -82,97 +82,89 @@ export function BasicTextsPresentTense() {
     <RouteSizeControlBox
       style={{
         backgroundColor: alwaysWhite(),
-        padding: "0.5rem",
+        padding: "1rem",
         height: "max-content",
         marginBottom: "1rem",
+        // maxWidth: "1500px",
       }}
     >
       <HOne>Basic Texts</HOne>
-      <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <div>
-          <RouteDiv
-            style={{
-              margin: "0 1rem",
-              maxWidth: "800px",
-              maxHeight: "700px",
-              padding: "1rem",
-              overflow: "auto",
-            }}
-          >
-            <HThree>Presentations</HThree>
-            {presentations.map((presentation, index) => {
-              return (
-                <div style={{ marginBottom: "3rem" }} key={index}>
-                  <HTwo style={{ fontSize: "1.3rem" }}>
-                    {presentation.title}
-                  </HTwo>
-                  <p
-                    style={{
-                      color: alwaysBlack(),
-                      padding: "1.1rem",
-                      fontSize: "1.2rem",
-                    }}
-                  >
-                    {presentation.text}
-                  </p>
-                  <iframe
-                    width="100%"
-                    height="200"
-                    scrolling="no"
-                    frameBorder="no"
-                    allow="autoplay"
-                    src={presentation.srcAudio}
-                    title="Exercise Audio"
-                  ></iframe>
-                </div>
-              );
-            })}
-            <HThree>Professions</HThree>
-            <iframe
-              width="100%"
-              height="200"
-              scrolling="no"
-              frameBorder="no"
-              allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639920315&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-              title="Professiona Audio"
-            ></iframe>
-            {professions.map((presentation, index) => {
-              return (
-                <div style={{ marginBottom: "3rem" }} key={index}>
-                  <HTwo style={{ fontSize: "1.3rem" }}>
-                    {presentation.title}
-                  </HTwo>
-                  <p
-                    style={{
-                      color: alwaysBlack(),
-                      padding: "1.1rem",
-                      fontSize: "1.2rem",
-                    }}
-                  >
-                    {presentation.text}
-                  </p>
-                </div>
-              );
-            })}
-          </RouteDiv>
-        </div>
-        <RouteSizeControlBox>
-          <BackToHomePage />
-          <textarea
-            name=""
-            id=""
-            style={{
-              minWidth: "750px",
-              minHeight: "620px",
-              padding: "1rem",
-              backgroundColor: primaryColor(),
-              color: textPrimaryColorContrast(),
-              fontSize: "1.5rem",
-            }}
-          />
-        </RouteSizeControlBox>
+      <BackToHomePage />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <HThree>Presentations</HThree>
+        {presentations.map((presentation, index) => {
+          return (
+            <div style={{ marginBottom: "3rem" }} key={index}>
+              <HTwo style={{ fontSize: "1.3rem" }}>{presentation.title}</HTwo>
+              <p
+                style={{
+                  color: alwaysBlack(),
+                  padding: "1.1rem",
+                  fontSize: "1.2rem",
+                }}
+              >
+                {presentation.text}
+              </p>
+              <iframe
+                width="100%"
+                height="100"
+                scrolling="no"
+                frameBorder="no"
+                allow="autoplay"
+                src={presentation.srcAudio}
+                title="Exercise Audio"
+              />
+              <textarea
+                name=""
+                id=""
+                style={{
+                  width: "98%",
+                  minHeight: "200px",
+                  padding: "1rem",
+                  color: primaryColor(),
+                  backgroundColor: textPrimaryColorContrast(),
+                  fontSize: "1.5rem",
+                }}
+              />
+            </div>
+          );
+        })}
+        <HThree>Professions</HThree>
+        <iframe
+          width="100%"
+          height="200"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1639920315&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false"
+          title="Professiona Audio"
+        />
+        {professions.map((presentation, index) => {
+          return (
+            <div style={{ marginBottom: "3rem" }} key={index}>
+              <HTwo style={{ fontSize: "1.3rem" }}>{presentation.title}</HTwo>
+              <p
+                style={{
+                  color: alwaysBlack(),
+                  padding: "1.1rem",
+                  fontSize: "1.2rem",
+                }}
+              >
+                {presentation.text}
+              </p>
+            </div>
+          );
+        })}
       </div>
+      {/* <RouteSizeControlBox>
+         
+        </RouteSizeControlBox> */}
     </RouteSizeControlBox>
   );
 }
