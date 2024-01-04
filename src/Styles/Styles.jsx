b7c214bd3237689ad1e49c5727d40891feaf9df3
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-01-04 16:58:21 -0300

    *

--- src/Styles/Styles.jsx
+++ src/Styles/Styles.jsx
@@ -7,7 +7,6 @@ export const primaryColor = () => "#00132b"; //Cor primária da marc
 export const secondaryColor = () => " #54bf08"; // Cor secundária da marca
 export const textPrimaryColorContrast = () => "#fff"; //Cor pra ficar atrás da primária
 export const textSecondaryColorContrast = () => "#fff"; // Cor pra ficar atrás da secundária
-
 // Fixed
 export const darkGreyColor = () => "#333";
 export const lightGreyColor = () => "#ddd";
@@ -15,7 +14,6 @@ export const transparentWhite = () => "rgba(255, 255, 255, 0.7)";
 export const transparentBlack = () => "rgba(1, 1, 1, 0.7)";
 export const alwaysWhite = () => "#fff";
 export const alwaysBlack = () => "#1a1a1a";
-
 // teste partner
 // export const primaryColor = () => "#68006b"; //Cor primária da marca
 // export const secondaryColor = () => " #b995ce"; //Cor secundária da marca
