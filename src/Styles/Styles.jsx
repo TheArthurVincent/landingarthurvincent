03ff1d89af6521b90e6cb31c79e74b91f1b5776d
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-01-04 17:24:16 -0300

    *

--- src/Styles/Styles.jsx
+++ src/Styles/Styles.jsx
@@ -1,23 +1,71 @@
-export const themePartner = {};
-
-// #00132b #54bf08 #0203AD #FACE0A #00a0a0 #a00030 #19D8FA #5b6b13 #86304a
-
 // Vincent --> Partner #1
-export const primaryColor = () => "#00132b"; //Cor primária da marc
-export const secondaryColor = () => " #54bf08"; // Cor secundária da marca
-export const textPrimaryColorContrast = () => "#fff"; //Cor pra ficar atrás da primária
-export const textSecondaryColorContrast = () => "#fff"; // Cor pra ficar atrás da secundária
-
+export const primaryColor = () => "#00132b";
+export const secondaryColor = () => " #54bf08";
+export const textPrimaryColorContrast = () => "#fff";
+export const textSecondaryColorContrast = () => "#fff";
 // Fixed
-export const darkGreyColor = () => "#333";
-export const lightGreyColor = () => "#ddd";
+export const darkGreyColor = () => "#222";
+export const lightGreyColor = () => "#e5e5e5";
 export const transparentWhite = () => "rgba(255, 255, 255, 0.7)";
 export const transparentBlack = () => "rgba(1, 1, 1, 0.7)";
 export const alwaysWhite = () => "#fff";
 export const alwaysBlack = () => "#1a1a1a";
 
-// teste partner
-// export const primaryColor = () => "#add"; //Cor primária da marca
-// export const secondaryColor = () => " #add"; //Cor secundária da marca
-// export const textPrimaryColorContrast = () => "#111"; //Cor pra ficar atrás da primária
-// export const textSecondaryColorContrast = () => "#111"; // Cor pra ficar atrás da secundária
+/*
+    Stellar: Refere-se a tudo relacionado a estrelas, incluindo seu brilho, características e propriedades.
+    Lunar: Relacionado à Lua. Este termo refere-se a tudo o que está associado à Lua.
+    Planetary: Envolvendo planetas. Este termo abrange aspectos relacionados aos planetas do nosso sistema solar ou outros sistemas planetários.
+    Interplanetary: Refere-se a tudo entre planetas. Pode incluir o espaço interestelar e os objetos que existem entre os planetas.
+    Nebulous: Relacionado a nebulosas, que são grandes nuvens de gás e poeira interestelares.
+    Galactic: Relacionado a galáxias. Este termo abrange tudo relacionado a galáxias, incluindo sua estrutura, composição e características.
+    Cosmic: Envolve o cosmos como um todo. Pode se referir a tudo que existe no universo, independentemente de sua forma ou composição específica.
+    Universal: Refere-se ao universo como um todo. Este termo é usado para descrever algo que é válido ou aplicável em todo o universo.
+*/
+
+// Stellar
+// export const primaryColor = () => "#add000";
+// export const secondaryColor = () => "#add555";
+// export const textPrimaryColorContrast = () => "#111";
+// export const textSecondaryColorContrast = () => "#111";
+
+// Lunar
+// export const primaryColor = () => "#456789";
+// export const secondaryColor = () => "#789abc";
+// export const textPrimaryColorContrast = () => "#fff";
+// export const textSecondaryColorContrast = () => "#fff";
+
+// Planetary
+// export const primaryColor = () => "#FACE0A";
+// export const secondaryColor = () => "#ffdd99";
+// export const textPrimaryColorContrast = () => "#333";
+// export const textSecondaryColorContrast = () => "#333";
+
+// Interplanetary
+// export const primaryColor = () => "#663399";
+// export const secondaryColor = () => "#9966cc";
+// export const textPrimaryColorContrast = () => "#fff";
+// export const textSecondaryColorContrast = () => "#fff";
+
+// Nebulous
+// export const primaryColor = () => "#996699";
+// export const secondaryColor = () => "#556699";
+// export const textPrimaryColorContrast = () => "#fff";
+// export const textSecondaryColorContrast = () => "#fff";
+
+// Galactic
+// export const primaryColor = () => "#550000";
+// export const secondaryColor = () => "#990000";
+// export const textPrimaryColorContrast = () => "#fff";
+// export const textSecondaryColorContrast = () => "#fff";
+
+// Cosmic
+// export const primaryColor = () => "#994564";
+// export const secondaryColor = () => "#554564";
+// export const textPrimaryColorContrast = () => "#fff";
+// export const textSecondaryColorContrast = () => "#fff";
+
+// Universal
+// export const primaryColor = () => "#996600";
+// export const secondaryColor = () => "#556600";
+// export const textPrimaryColorContrast = () => "#fff";
+// export const textSecondaryColorContrast = () => "#fff";
