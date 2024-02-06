e2605e482766cfef9309538ba377749955dd5810
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-02-06 15:36:10 -0300

    *

--- src/Routes/EnglishActivities/ClassesListActivities.jsx
+++ src/Routes/EnglishActivities/ClassesListActivities.jsx
@@ -4,15 +4,20 @@ export const activityRoutes = [
     url: "https://vimeo.com/910017704?share=copy",
     img: "https://s2.glbimg.com/55jH6PpCs7cjAib0WyGWhn55rRQ=/600x0/filters:quality(70)/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2022/x/o/HJSOT9RaO2dpksBnqOog/2020-04-28-will-smith.jpeg",
     questions: [
-      "Does Will like to be famous? (Worth 100 points)",
-      "Why didn't Will call his wife to come pick him up? (Worth 100 points)",
-      "How did Will infer that the man he saw would be a Fresh Prince Fan? (Worth 100 points)",
+      "Does Will like to be famous?",
+      "Why didn't Will call his wife to come pick him up?",
+      "How did Will infer that the man he saw would be a Fresh Prince Fan?",
     ],
   },
   {
-    title: "Phrasal verbs",
-    url: "google.com",
-    img: "https://ik.imagekit.io/vjz75qw96/assets/favicon.png?updatedAt=1697559035198",
+    title: "How to Make Learning as Addictive as Social Media",
+    url: "https://www.youtube.com/watch?v=P6FORpg0KVo",
+    img: "https://blog.cenatcursos.com.br/wp-content/uploads/2018/08/ted-logo-fb.png",
+    questions: [
+      "Where is the speaker from?",
+      "How did he describe his country to make it relatable to Americans?",
+      "What is his approach to learning?",
+    ],
   },
 ];
 
--- src/Routes/EnglishActivities/EnglishActivity.jsx
+++ src/Routes/EnglishActivities/EnglishActivity.jsx
@@ -19,36 +19,39 @@ export default function EnglishActivity({ headers, title, url, questions }) {
       <RouteDiv>
         <HOne>{title}</HOne>
         <BackToHomePage />
-        {url && <IFrameVideoClass src={getVideoEmbedUrl(url)} />}
-        {questions && (
-          <>
-            <HTwo>Questions to discuss during the class</HTwo>
-            <div>
-              <ul>
-                {questions.map((item, index) => {
-                  return <li key={index}>{item}</li>;
-                })}
-              </ul>
-            </div>
-          </>
-        )}
-        <HTwo>Sentence Mining</HTwo>
-        <div>
-          Find 5 terms you didn't know and mine 3 different sentences for each
-          one of them. Use one of those 3 websites. Make sure to adapt the
-          sentences if they need more context, so that they make sense: (Worth
-          100 points each)
-          <br />
-          <a href="https://www.linguee.com/">Linguee</a>
-          <br />
-          <a href="https://youglish.com/">YouGlish</a>
-          <br />
-          <a href="https://context.reverso.net/traducao/">Reverso</a>
-        </div>
-        <HTwo>Transcribe the text</HTwo>
-        <div>
-          Transcribe 10 sentences contained in this video and bring it to the
-          class.
+        <div style={{ padding: "1rem" }}>
+          {url && <IFrameVideoClass src={getVideoEmbedUrl(url)} />}
+          {questions && (
+            <>
+              <HTwo>Questions to discuss during the class.</HTwo>
+              <div>
+                <ul>
+                  {questions.map((item, index) => {
+                    return <li key={index}>{item} | (Worth 100 points each)</li>;
+                  })}
+                </ul>
+              </div>
+            </>
+          )}
+          <HTwo>Sentence Mining</HTwo>
+          <div>
+            Find 5 terms you didn't know and mine 3 different sentences for each
+            one of them. <br />
+            Use one of those 3 websites.
+            <br /> Make sure to adapt the sentences if they need more context,
+            so that they make sense: (Worth 100 points each)
+            <br />
+            <a href="https://www.linguee.com/">Linguee</a>
+            <br />
+            <a href="https://youglish.com/">YouGlish</a>
+            <br />
+            <a href="https://context.reverso.net/traducao/">Reverso</a>
+          </div>
+          <HTwo>Transcribe the text</HTwo>
+          <div>
+            Transcribe at least 10 sentences contained in this video and bring it to the
+            class. (100 sentences each)
+          </div>
         </div>
       </RouteDiv>
     </RouteSizeControlBox>
