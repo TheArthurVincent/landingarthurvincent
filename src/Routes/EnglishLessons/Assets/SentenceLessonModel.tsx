d6df12407f0eff92800f152d0fd296d8c42f5672
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-05-21 07:45:52 -0300

    *

--- src/Routes/EnglishLessons/Assets/SentenceLessonModel.tsx
+++ src/Routes/EnglishLessons/Assets/SentenceLessonModel.tsx
@@ -13,53 +13,44 @@ export default function SentenceLessonModel({
   sentences,
 }: SentenceLessonModelProps) {
   return (
-    <div
+    <ul
       style={{
-        display: "flex",
-        margin: "0 0 10px 0",
+        padding: "0.5rem",
+        display: "grid",
+        gap: "0.8rem",
+        gridTemplateColumns: "1fr 1fr",
       }}
     >
-      <ul
-        style={{
-          padding: "0.5rem",
-          display: "grid",
-          gap: "0.8rem",
-          gridTemplateColumns: "1fr 1fr",
-        }}
-      >
-        {sentences &&
-          sentences.map((sentence: any, i: number) => (
-            <li
-              key={i}
+      {sentences &&
+        sentences.map((sentence: any, i: number) => (
+          <li
+            key={i}
+            style={{
+              listStyle: "none",
+              marginBottom: "10px",
+              boxShadow: "1px 1px 10px 1px #ddd",
+              padding: "5px",
+            }}
+          >
+            <strong
               style={{
-                listStyle: "none",
-                marginBottom: "10px",
-                boxShadow: "1px 1px 1px 1px #f1f1f1",
-                padding: "5px",
+                color: !sentence.portuguese ? secondaryColor() : primaryColor(),
               }}
             >
-              <strong
-                style={{
-                  color: !sentence.portuguese
-                    ? secondaryColor()
-                    : primaryColor(),
-                }}
-              >
-                {sentence.english}
-              </strong>
-              <button
-                className="audio-button"
-                onClick={() => readText(sentence.english)}
-              >
-                <i className="fa fa-volume-up" aria-hidden="true" />
-              </button>
-              <br />
-              <span style={{ fontStyle: "italic" }}>{sentence.portuguese}</span>
-              <textarea className="comments" rows={2} cols={1} />
-              <br />
-            </li>
-          ))}
-      </ul>
-    </div>
+              {sentence.english}
+            </strong>
+            <button
+              className="audio-button"
+              onClick={() => readText(sentence.english)}
+            >
+              <i className="fa fa-volume-up" aria-hidden="true" />
+            </button>
+            <br />
+            <span style={{ fontStyle: "italic" }}>{sentence.portuguese}</span>
+            <textarea className="comments" />
+            <br />
+          </li>
+        ))}
+    </ul>
   );
 }
