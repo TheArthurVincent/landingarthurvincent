2ed33883d15baf76143a8b7032615292bfe8999b
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-06-07 21:39:49 -0300

    *

--- src/Resources/Components/ItemsLibrary.tsx
+++ src/Resources/Components/ItemsLibrary.tsx
@@ -1,6 +1,11 @@
-import React from "react";
 import styled from "styled-components";
-import { alwaysWhite, primaryColor, primaryColor2 } from "../../Styles/Styles";
+import {
+  alwaysWhite,
+  primaryColor,
+  primaryColor2,
+  secondaryColor,
+  secondaryColor2,
+} from "../../Styles/Styles";
 
 interface ButtonProps {
   firstcolor?: string;
@@ -8,11 +13,71 @@ interface ButtonProps {
   textcolor?: string;
 }
 
+interface ArvinButtonProps {
+  type?: string;
+}
+
+export const ArvinButton = styled.button<ArvinButtonProps>`
+  background: linear-gradient(
+    to left,
+    ${(props) =>
+        props.type === "red"
+          ? "red"
+          : props.type === "green"
+          ? secondaryColor()
+          : primaryColor()}
+      0%,
+    ${(props) =>
+        props.type === "red"
+          ? "#fa7a71"
+          : props.type === "green"
+          ? secondaryColor2()
+          : primaryColor2()}
+      50%
+  );
+  color: ${alwaysWhite()};
+  padding: 5px 1.2rem;
+  font-family: Athiti;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  display: inline;
+  max-width: fit-content;
+  font-weight: 600;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    background: linear-gradient(
+      to left,
+      ${(props) =>
+          props.type === "red"
+            ? "red"
+            : props.type === "green"
+            ? secondaryColor()
+            : primaryColor()}
+        0%,
+      ${(props) =>
+          props.type === "red"
+            ? "#FA7A71"
+            : props.type === "green"
+            ? secondaryColor2()
+            : primaryColor2()}
+        100%
+    );
+    box-shadow: 1px 1px 10px 1px #aaa;
+    border-radius: 12px;
+  }
+
+  &:active {
+    box-shadow: inset 1px 1px 10px 1px #777;
+  }
+`;
+
 export const MyButton = styled.button<ButtonProps>`
   background: linear-gradient(
     to left,
     ${(props) => props.firstcolor || primaryColor()} 0%,
-    ${(props) => props.secondcolor || props.firstcolor || primaryColor2()} 80%
+    ${(props) => props.secondcolor || props.firstcolor || primaryColor2()} 50%
   );
   color: ${(props) => props.textcolor || alwaysWhite()};
   padding: 5px 1.2rem;
@@ -26,15 +91,16 @@ export const MyButton = styled.button<ButtonProps>`
   transition: 0.3s ease-in-out;
   &:hover {
     background: linear-gradient(
-      to bottom,
+      to left,
       ${(props) => props.firstcolor || primaryColor()} 0%,
-      ${(props) => props.secondcolor || props.firstcolor || primaryColor2()} 90%
+      ${(props) => props.secondcolor || props.firstcolor || primaryColor2()} 100%
     );
-  
+
+    box-shadow: 1px 1px 10px 1px #aaa;
     border-radius: 12px;
   }
 
   &:active {
-    font-weight: 500;
+    box-shadow: inset 1px 1px 10px 1px #333;
   }
 `;
--- src/Routes/FlashCards/FlashCards.tsx
+++ src/Routes/FlashCards/FlashCards.tsx
@@ -1,16 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import {
+  HOne,
   RouteDiv,
   RouteSizeControlBox,
 } from "../../Resources/Components/RouteBox";
 import { useUserContext } from "../../Application/SelectLanguage/SelectLanguage";
 import Helmets from "../../Resources/Helmets";
 import { HeadersProps } from "../../Resources/types.universalInterfaces";
+import { readText } from "../EnglishLessons/Assets/Functions/FunctionLessons";
+import { ArvinButton } from "../../Resources/Components/ItemsLibrary";
+import { secondaryColor, secondaryColor2 } from "../../Styles/Styles";
 
 export function FlashCards({ headers }: HeadersProps) {
+  const reviewDay = new Date();
+  useEffect(() => {
+    console.log(reviewDay);
+  }, [reviewDay]);
+
+  const cards = [
+    {
+      front: { text: "Hi", side: "front", language: "en" },
+      back: { text: "Oi", side: "back", language: "pt" },
+      reviewDate: new Date(),
+    },
+    // {
+    //   front: { text: "Hi", side: "front", language: "en" },
+    //   back: { text: "Oi", side: "back", language: "pt" },
+    //   reviewDate: new Date(),
+    // },
+    // {
+    //   front: { text: "Hi", side: "front", language: "en" },
+    //   back: { text: "Oi", side: "back", language: "pt" },
+    //   reviewDate: new Date(),
+    // },
+    // {
+    //   front: { text: "Hi", side: "front", language: "en" },
+    //   back: { text: "Oi", side: "back", language: "pt" },
+    //   reviewDate: new Date(),
+    // },
+  ];
+
   return (
     <RouteSizeControlBox className="smooth">
-      <RouteDiv>oioioioioioioioioioi</RouteDiv>
+      <RouteDiv>
+        <HOne>Flash Cards</HOne>
+        {cards.map((card, index) => (
+          <div key={index}>
+            <div
+              style={{ maxWidth: "10rem", margin: "auto", textAlign: "center" }}
+            >
+              <div style={{ borderBottom: "1px solid #ccc", padding: "2rem" }}>
+                {card.front.text}
+                {card.front.language === "en" && (
+                  <button
+                    className="audio-button"
+                    onClick={() => readText(card.front.text, true)}
+                  >
+                    <i className="fa fa-volume-up" aria-hidden="true" />
+                  </button>
+                )}
+              </div>
+              <div style={{ padding: "1rem" }}>
+                {card.back.text}
+                {card.back.language === "en" && (
+                  <button
+                    className="audio-button"
+                    onClick={() => readText(card.back.text, true)}
+                  >
+                    <i className="fa fa-volume-up" aria-hidden="true" />
+                  </button>
+                )}
+              </div>
+            </div>{" "}
+            <div
+              style={{ justifyContent: "center", display: "flex", gap: "5px" }}
+            >
+              <div>
+                <ArvinButton type="red">Hard</ArvinButton>
+                <p style={{ fontSize: "10px", textAlign: "center" }}>10 dias</p>
+              </div>
+              <div>
+                <ArvinButton type="navy">Medium</ArvinButton>
+                <p style={{ fontSize: "10px", textAlign: "center" }}>10 dias</p>
+              </div>
+              <div>
+                <ArvinButton type="green">Easy</ArvinButton>
+                <p style={{ fontSize: "10px", textAlign: "center" }}>10 dias</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </RouteDiv>
       <Helmets text="Flash Cards Review" />
     </RouteSizeControlBox>
   );
