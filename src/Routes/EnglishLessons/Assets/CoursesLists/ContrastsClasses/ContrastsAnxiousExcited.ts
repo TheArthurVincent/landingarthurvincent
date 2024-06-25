700cd9b7a79c2e3838129efce16c3b7f19b16165
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-06-25 10:21:07 -0300

    *

--- src/Routes/EnglishLessons/Assets/CoursesLists/ContrastsClasses/ContrastsAnxiousExcited.ts
+++
@@ -1,9 +0,0 @@
-export const ContrastsAnxiousExcited = [
-  {
-    title: "Anxious vs. Excited (ansiedade)",
-    order: 1,
-    type: "When English contrasts with Portuguese",
-    description: "This class will be ready soon.",
-    elements: [],
-  },
-];
--- src/Routes/EnglishLessons/Assets/CoursesLists/ContrastsClasses/ContrastsIntroduction.ts
+++
@@ -1,9 +0,0 @@
-export const ContrastsIntroduction = [
-  {
-    title: "Introduction Contrasts",
-    order: 0,
-    type: "When English contrasts with Portuguese",
-    description: "This class will be ready soon.",
-    elements: [],
-  },
-];
