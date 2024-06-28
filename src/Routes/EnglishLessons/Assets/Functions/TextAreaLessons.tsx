b2df20ef9d38eea3e8a33f7ad9e0b88926a73812
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-06-28 09:17:28 -0300

    *

--- src/Routes/EnglishLessons/Assets/Functions/TextAreaLessons.tsx
+++ src/Routes/EnglishLessons/Assets/Functions/TextAreaLessons.tsx
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function TextAreaLesson() {
   const [value, setValue] = useState("");
-  const [className, setClassName] = useState("no-print");
+  const [permissions, setPermissions] = useState<string>("");
+
+  useEffect(() => {
+    const user = localStorage.getItem("loggedIn");
+    if (user) {
+      const { permissions } = JSON.parse(user);
+      setPermissions(permissions);
+    }
+  }, []);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newValue = event.target.value;
     setValue(newValue);
-    setClassName(newValue ? "" : "no-print");
   };
 
   return (
     <textarea
+      style={{ display: permissions === "superadmin" ? "block" : "none" }}
       className={`comments ${value == "" ? "no-print" : ""}`}
       value={value}
       onChange={handleChange}
