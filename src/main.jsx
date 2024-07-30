2a298c9ea6a275e9104a1fc5d3bb52959eb008e1
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-07-30 19:40:13 -0300

    *

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Styles/styles.css";
 import App from "./App";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
