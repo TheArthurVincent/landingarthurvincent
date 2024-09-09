e9bb2abb5b17962abc28e64c5ee602401c64d7e0
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-09-09 15:50:32 -0300

    +

--- src/Routes/MyProfile/MyProfile.tsx
+++ src/Routes/MyProfile/MyProfile.tsx
@@ -23,6 +23,7 @@ export function MyProfile({ headers }: HeadersProps) {
   const [user, setUser] = useState<User>({} as User);
   const [newPassword, setNewPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
 
   const actualHeaders = headers || {};
 
@@ -50,11 +51,13 @@ export function MyProfile({ headers }: HeadersProps) {
   };
 
   useEffect(() => {
+    setLoading(true);
     try {
       const getLoggedUser: User = JSON.parse(
         localStorage.getItem("loggedIn") || ""
       );
       setUser(getLoggedUser);
+      setLoading(false);
     } catch (e) {
       console.log(e);
       onLoggOut();
@@ -80,144 +83,148 @@ export function MyProfile({ headers }: HeadersProps) {
       {headers ? (
         <RouteDiv className="smooth grid-flex">
           <Helmets text="My Profile" />
-          <div>
-            <ul
-              style={{
-                display: "grid",
-                gap: "10px",
-                color: alwaysBlack(),
-                padding: "1rem",
-                backgroundColor: "#f7f9fc",
-                borderRadius: "10px",
-                boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              <ArvinButton
-                onClick={() => updateInfo(user.id, headers)}
-                color="navy"
-                style={{
-                  backgroundColor: "#1a73e8",
-                  color: "#fff",
-                  padding: "0.5rem 1rem",
-                  borderRadius: "8px",
-                  cursor: "pointer",
-                  transition: "opacity 0.3s ease"
-                }}
-              >
-                <i className="fa fa-refresh" aria-hidden="true" />
-              </ArvinButton>
-              <HOne>{UniversalTexts.myProfile}</HOne>
-              <div
-                style={{
-                  display: "flex",
-                  gap: "1.5rem",
-                  alignItems: "center",
-                  paddingBottom: "1.5rem",
-                }}
-              >
-                <img
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <>
+              <div>
+                <ul
                   style={{
-                    width: "8rem",
-                    height: "8rem",
-                    borderRadius: "50%",
-                    objectFit: "cover",
+                    display: "grid",
+                    gap: "10px",
+                    color: alwaysBlack(),
+                    padding: "1rem",
+                    backgroundColor: "#f7f9fc",
+                    borderRadius: "10px",
                     boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
                   }}
-                  src={user.picture}
-                  alt="Profile"
-                />
-                <div>
-                  {myProfileList.map((item, index) => (
-                    <li
-                      key={index}
+                >
+                  <ArvinButton
+                    onClick={() => updateInfo(user.id, headers)}
+                    color="navy"
+                    style={{
+                      backgroundColor: "#1a73e8",
+                      color: "#fff",
+                      padding: "0.5rem 1rem",
+                      borderRadius: "8px",
+                      cursor: "pointer",
+                      transition: "opacity 0.3s ease",
+                    }}
+                  >
+                    <i className="fa fa-refresh" aria-hidden="true" />
+                  </ArvinButton>
+                  <HOne>{UniversalTexts.myProfile}</HOne>
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "1.5rem",
+                      alignItems: "center",
+                      paddingBottom: "1.5rem",
+                    }}
+                  >
+                    <img
                       style={{
-                        listStyle: "none",
-                        padding: "0.5rem 0",
-                        fontSize: "1rem",
-                        lineHeight: "1.5",
-                        color: "#333",
+                        width: "8rem",
+                        height: "8rem",
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
                       }}
-                    >
-                      <SpanDisapear>
-                        <b>{item.title}: </b>
-                      </SpanDisapear>
-                      {item.link ? (
-                        <NavLink
-                          to={item.link}
+                      src={user.picture}
+                      alt="Profile"
+                    />
+                    <div>
+                      {myProfileList.map((item, index) => (
+                        <li
+                          key={index}
                           style={{
-                            color: "#1a73e8",
-                            textDecoration: "underline",
+                            listStyle: "none",
+                            padding: "0.5rem 0",
+                            fontSize: "1rem",
+                            lineHeight: "1.5",
+                            color: "#333",
                           }}
                         >
-                          Click here
-                        </NavLink>
-                      ) : (
-                        <span>{item.data}</span>
-                      )}
-                    </li>
-                  ))}
-                </div>
+                          <SpanDisapear>
+                            <b>{item.title}: </b>
+                          </SpanDisapear>
+                          {item.link ? (
+                            <NavLink
+                              to={item.link}
+                              style={{
+                                color: "#1a73e8",
+                                textDecoration: "underline",
+                              }}
+                            >
+                              Click here
+                            </NavLink>
+                          ) : (
+                            <span>{item.data}</span>
+                          )}
+                        </li>
+                      ))}
+                    </div>
+                  </div>
+                </ul>
               </div>
-            </ul>
-          </div>
-          <div
-            style={{
-              padding: "2rem",
-              backgroundColor: "#fff",
-              borderRadius: "10px",
-              boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-              marginTop: "2rem",
-            }}
-          >
-            <form style={{ textAlign: "center" }}>
-              <HOne>{UniversalTexts.newPassword}</HOne>
-              <input
-                value={newPassword}
-                onChange={(event) => setNewPassword(event.target.value)}
-                placeholder={UniversalTexts.newPassword}
-                type="password"
-                className="inputs-style"
+              <div
                 style={{
-                  marginBottom: "1rem",
-                  padding: "0.75rem",
-                  width: "100%",
-                  borderRadius: "8px",
-                  border: "1px solid #ccc",
-                  fontSize: "1rem",
+                  padding: "2rem",
+                  backgroundColor: "#fff",
+                  borderRadius: "10px",
+                  boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+                  marginTop: "2rem",
                 }}
-              />
-              <input
-                value={confirmPassword}
-                onChange={(event) =>
-                  setConfirmPassword(event.target.value)
-                }
-                placeholder={UniversalTexts.confirmNewPassword}
-                type="password"
-                className="inputs-style"
-                style={{
-                  marginBottom: "1rem",
-                  padding: "0.75rem",
-                  width: "100%",
-                  borderRadius: "8px",
-                  border: "1px solid #ccc",
-                  fontSize: "1rem",
-                }}
-              />
-              <Button
-                style={{
-                  color: "#fff",
-                  backgroundColor: "#138017",
-                  padding: "0.75rem 2rem",
-                  borderRadius: "8px",
-                  fontSize: "1rem",
-                  cursor: "pointer",
-                }}
-                onClick={() => editStudentPassword()}
               >
-                {UniversalTexts.save}
-              </Button>
-            </form>
-          </div>
+                <form style={{ textAlign: "center" }}>
+                  <HOne>{UniversalTexts.newPassword}</HOne>
+                  <input
+                    value={newPassword}
+                    onChange={(event) => setNewPassword(event.target.value)}
+                    placeholder={UniversalTexts.newPassword}
+                    type="password"
+                    className="inputs-style"
+                    style={{
+                      marginBottom: "1rem",
+                      padding: "0.75rem",
+                      width: "100%",
+                      borderRadius: "8px",
+                      border: "1px solid #ccc",
+                      fontSize: "1rem",
+                    }}
+                  />
+                  <input
+                    value={confirmPassword}
+                    onChange={(event) => setConfirmPassword(event.target.value)}
+                    placeholder={UniversalTexts.confirmNewPassword}
+                    type="password"
+                    className="inputs-style"
+                    style={{
+                      marginBottom: "1rem",
+                      padding: "0.75rem",
+                      width: "100%",
+                      borderRadius: "8px",
+                      border: "1px solid #ccc",
+                      fontSize: "1rem",
+                    }}
+                  />
+                  <Button
+                    style={{
+                      color: "#fff",
+                      backgroundColor: "#138017",
+                      padding: "0.75rem 2rem",
+                      borderRadius: "8px",
+                      fontSize: "1rem",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => editStudentPassword()}
+                  >
+                    {UniversalTexts.save}
+                  </Button>
+                </form>
+              </div>
+            </>
+          )}
         </RouteDiv>
       ) : (
         <RouteDiv>Nenhum usuário logado</RouteDiv>
