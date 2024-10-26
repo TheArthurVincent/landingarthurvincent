6752f0922fd3f46cb7a4e1c4669780a8c2e5d920
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-10-26 09:52:06 -0300

    d

--- src/Routes/EnglishLessons/Assets/Functions/FunctionLessons.ts
+++ src/Routes/EnglishLessons/Assets/Functions/FunctionLessons.ts
@@ -1,39 +1,39 @@
 export const readText = (text: string, restart: boolean, lang?: string) => {
   if ("speechSynthesis" in window) {
     const synth = window.speechSynthesis;
-
     if (!synth) {
       console.error("speechSynthesis não está disponível.");
       return;
     }
-    // Limpa qualquer fala em andamento
     synth.cancel();
 
     const utterance = new SpeechSynthesisUtterance(text);
 
     // Definir o idioma
-    utterance.lang = lang === "pt" ? "pt-BR" : 
-                      lang === "fr" ? "fr-FR" : 
-                      lang === "it" ? "it-IT" : 
-                      lang === "de" ? "de-DE" : "en-US";
+    switch (lang) {
+      case "pt":
+        utterance.lang = "pt-BR";
+        break;
+      case "fr":
+        utterance.lang = "fr-FR";
+        break;
+      case "it":
+        utterance.lang = "it-IT";
+        break;
+      case "de":
+        utterance.lang = "de-DE";
+        break;
+      case "en":
+      default:
+        utterance.lang = "en-US";
+        break;
+    }
 
     // Eventos de controle
     utterance.onstart = () => console.log("Leitura iniciada.");
     utterance.onend = () => console.log("Leitura finalizada.");
-    utterance.onerror = (e) => {
-      console.error("Erro na leitura:", e);
+    utterance.onerror = (e) => console.error("Erro na leitura:", e);
 
-      // Se a leitura foi interrompida, tenta reiniciar
-      if (e.error === "interrupted" && restart) {
-        console.warn("Leitura interrompida, tentando reiniciar...");
-        synth.cancel();
-        synth.speak(utterance);
-      } else {
-        console.log(`Ocorreu um erro: ${e.error}`);
-      }
-    };
-
-    // Lógica de controle de fala
     if (restart) {
       synth.cancel();
       synth.speak(utterance);
@@ -50,7 +50,6 @@ export const readText = (text: string, restart: boolean, lang?: string) => {
   }
 };
 
-// Função para pausar a fala
 export const pauseSpeech = () => {
   if ("speechSynthesis" in window) {
     const synth = window.speechSynthesis;
