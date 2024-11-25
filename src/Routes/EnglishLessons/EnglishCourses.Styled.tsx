60ffc8304381ffea21221fdc5d8ad89927a601cc
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-11-25 13:02:06 -0300

    5

--- src/Routes/EnglishLessons/EnglishCourses.Styled.tsx
+++ src/Routes/EnglishLessons/EnglishCourses.Styled.tsx
@@ -27,6 +27,8 @@ export const CourseItem = styled.div`
 `;
 export const CourseCard = styled.div`
   display: flex;
+  padding-top: 5px;
+  padding-bottom: 5px;
   align-items: center;
   justify-content: space-between;
   background-color: #f5f5f5;
