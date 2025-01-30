89d5ed47ec8c4a96f659b153631e11c5158c4584
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-01-30 10:18:28 -0300

    d

--- src/Routes/FlashCards/FlashCardsComponents/ReviewFlashCards.tsx
+++ src/Routes/FlashCards/FlashCardsComponents/ReviewFlashCards.tsx
@@ -244,11 +244,7 @@ const ReviewFlashCards = ({ headers, onChange, change }: FlashCardsPropsRv) => {
       {/*  */}
       {/*  */}
       {/*  */}
-      {/*  */}
-      {/*  */}
       {/* <Countdown targetDate={new Date("2024-12-31T17:59:59")} text="You have until December 31st 2024 - 6PM to score 10 points per card!" /> */}
-      {/*  */}
-      {/*  */}
       {/*  */}
       {/*  */}
       {/*  */}
