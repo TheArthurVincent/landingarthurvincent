be77692df3964101d48c4a85491c64d5b5d47c33
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-01-30 10:08:25 -0300

    d

--- src/Routes/Ranking/Ranking.tsx
+++ src/Routes/Ranking/Ranking.tsx
@@ -80,7 +80,7 @@ export default function Ranking({ headers }: HeadersProps) {
     setValue(newValue);
   };
 
-  const targetDate = new Date("2025-01-31T18:00:00");
+  const targetDate = new Date("2025-01-31T22:00:00");
 
   return (
     <RouteDiv>
