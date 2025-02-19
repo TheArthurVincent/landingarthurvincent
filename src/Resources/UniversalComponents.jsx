9761e9ab716125b17a6c5180442ff1aa7d94d090
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-02-19 11:36:42 -0300

    d

--- src/Resources/UniversalComponents.jsx
+++ src/Resources/UniversalComponents.jsx
@@ -762,24 +762,25 @@ export const ButtonButton = (text) => {
   );
 };
 
-// export function isDev() {
-//   if (window.location.hostname === "localhost") {
-//     return "http://localhost:3502";
-//   } else if (
-//     window.location.hostname === "arvin-staging.9kwq6c.easypanel.host"
-//   ) {
-//     return "https://arvin-backstagin.9kwq6c.easypanel.host";
-//   } else {
-//     return "https://apiprod.arthurvincent.com.br";
-//   }
-// }
 export function isDev() {
   if (window.location.hostname === "localhost") {
     return "http://localhost:3502";
+  } else if (
+    window.location.hostname === "arvin-staging.9kwq6c.easypanel.host"
+  ) {
+    return "https://arvin-backprod.9kwq6c.easypanel.host";
   } else {
     return "https://arvin-backstagin.9kwq6c.easypanel.host";
   }
 }
+
+// export function isDev() {
+//   if (window.location.hostname === "localhost") {
+//     return "http://localhost:3502";
+//   } else {
+//     return "https://arvin-backstagin.9kwq6c.easypanel.host";
+//   }
+// }
 
 export const backDomain = isDev();
 export const updateInfo = async (id, headers) => {
