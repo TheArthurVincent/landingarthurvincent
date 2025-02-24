acd5d5ddb331ac4529aaddb4e37f92827f6a071b
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-02-24 20:49:10 -0300

    d

--- src/Routes/EnglishLessons/Assets/LessonsModels/ExerciseLessonModelExercise.tsx
+++ src/Routes/EnglishLessons/Assets/LessonsModels/ExerciseLessonModelExercise.tsx
@@ -1,35 +1,72 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { MyHeadersType } from "../../../../Resources/types.universalInterfaces";
-import HTMLJustWrite from "../../../../Resources/Components/HTMLJustWrite";
+
 interface ExerciseLessonModelLessonProps {
   headers: MyHeadersType | null;
-  item: any;
+  item: string[];
 }
 
 export default function ExerciseLessonModelLesson({
   headers,
   item,
 }: ExerciseLessonModelLessonProps) {
-  const [newHWDescription, setNewHWDescription] = useState("");
-  const handleHWDescriptionChange = (htmlContent: any) => {
-    setNewHWDescription(htmlContent);
-  };
+  const textRef = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    if (textRef.current) {
+      // Função para atualizar o tamanho
+      const updateSize = () => {
+        if (textRef.current) {
+          setSize({
+            width: textRef.current.offsetWidth,
+            height: textRef.current.offsetHeight,
+          });
+        }
+      };
+
+      // Inicializa o tamanho
+      updateSize();
+
+      // Observer para mudanças no tamanho do conteúdo
+      const resizeObserver = new ResizeObserver(updateSize);
+      resizeObserver.observe(textRef.current);
+
+      return () => resizeObserver.disconnect();
+    }
+  }, [item]); // Atualiza quando os itens mudam
+
   return (
     <div
+      ref={textRef}
       style={{
         padding: "5px",
         margin: "10px 0",
+        position: "relative", // Garante que o iframe fique sobreposto corretamente
       }}
     >
       <ol>
-        {item.map((theitem: string, index: number) => {
-          return (
-            <li style={{ fontSize: "1.5rem" }} key={index}>
-              {theitem}
-            </li>
-          );
-        })}{" "}
+        {item.map((theitem: string, index: number) => (
+          <li style={{ fontSize: "1.5rem" }} key={index}>
+            {theitem}
+          </li>
+        ))}
       </ol>
+
+      {/* Iframe que bloqueia a seleção */}
+      <iframe
+        src="about:blank"
+        title="blocker"
+        className="absolute top-0 left-0 opacity-0 pointer-events-auto"
+        style={{
+          width: size.width,
+          height: size.height,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          border: "none",
+        }}
+      />
     </div>
   );
 }
